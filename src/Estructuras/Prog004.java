1f1d608f422864bebf63fa863be41b32917a1433
Eric Cuba <garralaser@gmail.com>
2020-06-30 16:52:52 -0500

    listo la 4

---
+++ src/Estructuras/Prog004.java
@@ -0,0 +1,15 @@
+
+package Estructuras;
+
+
+public class Prog004 {
+  public static void main(String[] args) {
+        int num =1;
+        String mensaje = " Cristo viene pronto - Prepárate";
+        while (num<=10) {   
+            System.out.println(mensaje);
+            num++;
+            
+        }
+    }   
+}
